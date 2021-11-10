cd778afc321d8c496495b858c58a2b57a3d525b2
Kakarot <57848836+GhzGarage@users.noreply.github.com>
2021-11-10 00:52:07 -0600

    lua54

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,13 +6,12 @@ version '1.0.0'
 
 ui_page 'html/index.html'
 
-client_scripts {
-    '@qb-core/import.lua',
-    'client/main.lua',
-}
+client_script 'client/main.lua'
 
 files {
     'html/index.html',
     'html/app.js',
     'html/style.css',
 }
+
+lua54 'yes'
